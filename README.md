8f59f325f4eeeace5e3da368b452b2fdda6f972a
Jonni Madekivi <jonni.madekivi@reaktor.com>
2020-01-19 20:41:48 +0200

    Switch to dark theme & add build instructions.

--- README.md
+++ README.md
@@ -1,19 +1,9 @@
-# Default project setup for Vaadin using Spring Boot
+# Production build
 
-This project can be used as a starting point to create your own Vaadin application with Spring Boot.
-It contains the required dependencies and configuration and sample code to get you started.
+`mvn clean package -Pproduction`
 
-The best way to create your own project based on this setup is [vaadin.com/start](https://vaadin.com/start/) - you can get only the necessary parts and choose the package naming you want to use.
+# Deploy
 
-Import the project to the IDE of your choosing as a Maven project.
+`scp target/jamote-*.jar nemo@10.0.0.88:`
 
-Run the application using `mvn spring-boot:run` or by running the `Application` class directly from your IDE.
-
-Open http://localhost:8080/ in your browser.
-
-If you want to run the application locally in the production mode, run `mvn spring-boot:run -Pproduction`.
-
-For documentation on using Vaadin and Spring, visit [vaadin.com/docs](https://vaadin.com/docs/v14/flow/spring/tutorial-spring-basic.html#add-vaadin-view-to-spring-boot-application)
-
-For more information on Vaadin, visit https://vaadin.com/.
-
+`ssh nemo@10.0.0.88 "/home/nemo/jdk/bin/java -jar jamote-*.jar"`
--- src/main/java/org/vaadin/jonni/jamote/MainView.java
+++ src/main/java/org/vaadin/jonni/jamote/MainView.java
@@ -13,14 +13,16 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 @Route
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+@Theme(value= Lumo.class, variant = Lumo.DARK)
 public class MainView extends VerticalLayout {
 
 	public MainView(@Autowired RestTemplate restTemplate) {
-
 		NetworkStatus networkStatus = restTemplate
 				.getForObject("http://10.0.0.41/YamahaExtendedControl/v1/system/getNetworkStatus", NetworkStatus.class);
 
@@ -28,15 +30,13 @@ public class MainView extends VerticalLayout {
 		stepField.setValue(10.0);
 
 		Button plusButton = new Button("+", e -> {
-			restTemplate.getForObject(
-					"http://10.0.0.41/YamahaExtendedControl/v1/main/setVolume?volume=up&step=" + stepField.getValue().intValue(),
-					NetworkStatus.class);
+			restTemplate.getForObject("http://10.0.0.41/YamahaExtendedControl/v1/main/setVolume?volume=up&step="
+					+ stepField.getValue().intValue(), NetworkStatus.class);
 			reportVolume(restTemplate);
 		});
 		Button minusButton = new Button("-", e -> {
-			restTemplate.getForObject(
-					"http://10.0.0.41/YamahaExtendedControl/v1/main/setVolume?volume=down&step=" + stepField.getValue().intValue(),
-					NetworkStatus.class);
+			restTemplate.getForObject("http://10.0.0.41/YamahaExtendedControl/v1/main/setVolume?volume=down&step="
+					+ stepField.getValue().intValue(), NetworkStatus.class);
 
 			reportVolume(restTemplate);
 
@@ -47,8 +47,8 @@ public class MainView extends VerticalLayout {
 	}
 
 	private void reportVolume(RestTemplate restTemplate) {
-		Status status = restTemplate
-				.getForObject("http://10.0.0.41/YamahaExtendedControl/v1/main/getStatus", Status.class);
+		Status status = restTemplate.getForObject("http://10.0.0.41/YamahaExtendedControl/v1/main/getStatus",
+				Status.class);
 		ActualVolume volume = status.getActualVolume();
 
 		Notification.show("" + volume.getValue() + " dB");
